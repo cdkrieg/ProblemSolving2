21511d7ab346786f44f30ebfadffce3d8d8d332a
cdkrieg <cdkrieg@outlook.com>
2022-03-02 17:33:56 -0600

    added function to check if an array of numbers form a sequence

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 "use strict"
-// //Happy Numbers
-// let happyNumber = "Happy Number!";
-// let sadNumber = "Not a Happy Number.";
-// let newNumber = 0;
+//Happy Numbers
+let happyNumber = "Happy Number!";
+let sadNumber = "Not a Happy Number.";
+let newNumber = 0;
 
-// function happyNumberchecker(number){
-//     if(number === 1){
-//         return happyNumber;
-//     }
-//     let numberString = number.toString();
-//     while(numberString.length > 1){
-//         for(let i = 0; i < numberString.length; i++){
-//         newNumber += Math.pow(parseInt(numberString[i]),2);
-//         }
-//         numberString = newNumber.toString();
-//         newNumber = 0;
-//     }
-//     if(parseInt(numberString) === 1){
-//         return happyNumber;
-//     } else {
-//         return sadNumber;
-//     }
-// }
-// let userNumber = happyNumberchecker(prompt("Enter a number to check if it is happy!"));
-// alert(userNumber);
+function happyNumberchecker(number){
+    if(number === 1){
+        return happyNumber;
+    }
+    let numberString = number.toString();
+    while(numberString.length > 1){
+        for(let i = 0; i < numberString.length; i++){
+        newNumber += Math.pow(parseInt(numberString[i]),2);
+        }
+        numberString = newNumber.toString();
+        newNumber = 0;
+    }
+    if(parseInt(numberString) === 1){
+        return happyNumber;
+    } else {
+        return sadNumber;
+    }
+}
+let userNumber = happyNumberchecker(prompt("Enter a number to check if it is happy!"));
+alert(userNumber);
 
-// //Prime Numbers
-// let prime = "Prime Number";
-// let notPrime = "Non-Prime Number"
-// function checkIfNumberPrime(number){
-//     if(number > 1 && (number === 2 || number === 3 )){
-//         return prime;
-//     } else if(number%2 === 0 || number%3 === 0 || number%5 === 0){
-//         return notPrime
-//     } else if(number <= 1){
-//         return notPrime;
-//     }
-//     for(let i = 7; i<number;i++){
-//         if(number%i === 0){
-//             return notPrime
-//         }
-//     } 
-//     return prime;
-// }
-// let userInput = checkIfNumberPrime(prompt("Enter a number to check if prime"));
-// alert(userInput);
+//Prime Numbers
+let prime = "Prime Number";
+let notPrime = "Non-Prime Number"
+function checkIfNumberPrime(number){
+    if(number > 1 && (number === 2 || number === 3 )){
+        return prime;
+    } else if(number%2 === 0 || number%3 === 0 || number%5 === 0){
+        return notPrime
+    } else if(number <= 1){
+        return notPrime;
+    }
+    for(let i = 7; i<number;i++){
+        if(number%i === 0){
+            return notPrime
+        }
+    } 
+    return prime;
+}
+let userInput = checkIfNumberPrime(prompt("Enter a number to check if prime"));
+alert(userInput);
 
-// //Fibonacci 
-// function fibonacciSequence(number){
-//     let fibonacciArray = [1,1]
-//         for(let i =0; fibonacciArray.length<number; i++){
-//             fibonacciArray.push(parseInt(fibonacciArray[i]) + parseInt(fibonacciArray[i+1]));
-//         }
-//         return fibonacciArray;
-// }
-// let sequence = fibonacciSequence(prompt("Enter number of numbers in Fibonacci sequence to display"));
-// alert(sequence);
+//Fibonacci 
+function fibonacciSequence(number){
+    let fibonacciArray = [1,1]
+        for(let i =0; fibonacciArray.length<number; i++){
+            fibonacciArray.push(parseInt(fibonacciArray[i]) + parseInt(fibonacciArray[i+1]));
+        }
+        return fibonacciArray;
+}
+let sequence = fibonacciSequence(prompt("Enter number of numbers in Fibonacci sequence to display"));
+alert(sequence);
 
 //Find numbers in an array that add up to a target value
 function findNumbersInArrayThatAddToTargetValue(number, array){
@@ -76,3 +76,21 @@ let userNumber = parseInt(prompt("Enter a target number"));
 let arrayOfNumbers = [2,11,7,21,16,34,6];
 alert(findNumbersInArrayThatAddToTargetValue(userNumber, arrayOfNumbers));
 
+//checking if a list of numbers form a sequence
+function checkForSequence(listOfNumbers){
+    let arrayOfDifferences= [];
+    listOfNumbers.sort((a,b) => (a-b));
+    for(let i = 0; i < listOfNumbers.length -1; i++){
+        let difference = parseInt(listOfNumbers[i+1] - parseInt(listOfNumbers[i]));
+        arrayOfDifferences.push(difference);
+    }
+    for(let i = 0; i < listOfNumbers.length -2; i++){
+        if(arrayOfDifferences[i] !== arrayOfDifferences[i+1]){
+            return false;
+        } 
+    }
+    return true;
+}
+let listOfNumbers = [5,7,3,11,9,1,13];
+let isSequence = checkForSequence(listOfNumbers);
+alert(`Is the array of numbers a sequence: ${isSequence}`);
